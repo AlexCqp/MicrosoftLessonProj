cd8f4ea1cc90df14ceec215c3d55bc66f22779af
Alex <acqp2tl@gmail.com>
2021-09-08 15:09:41 +0300

    readonly fix

--- Controllers/FoodController.cs
+++ Controllers/FoodController.cs
@@ -18,7 +18,7 @@ namespace ContosoPizza.Controllers
         /// Экземпляр класса службы данных
         /// </summary>
 
-        readonly private IFoodItemSender<Food> _foodSender;
+        private readonly IFoodItemSender<Food> _foodSender;
         /// <summary>
         /// Конструктор
         /// </summary>
